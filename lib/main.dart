9093154cb8faeddde37a576f81031f4f89ddd291
yared098 <fdessalew@gmail.com>
2025-02-24 12:10:43 +0300

    add text edit

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You havy times:',
             ),
             Text(
               '$_counter',
